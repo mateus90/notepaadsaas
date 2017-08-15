27af988c236e6697a9b9ef421033b3f621a0f5f7
logonrm <logonrm@fiap.com.br>
2017-08-15 20:52:24 -0300

    rota para busca por titulo

--- src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
+++ src/main/java/br/com/fiap/notepadsaas/controller/NotaController.java
@@ -2,15 +2,14 @@ package br.com.fiap.notepadsaas.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.notepadsaas.model.Nota;
 import br.com.fiap.notepadsaas.repository.NotaRepository;
 
+@CrossOrigin
 @RestController
+@RequestMapping(value = "/nota")
 public class NotaController {
 
 	private NotaRepository notaRepository;
@@ -27,6 +26,11 @@ public class NotaController {
 	@PostMapping(value = "/")
 	public void save(@RequestBody Nota nota) {
 		notaRepository.save(nota);
+	}
+
+	@GetMapping(value = "/titulo/{titulo}")
+	private List<Nota> findByTitulo(@PathVariable(value = "titulo") String q) {
+		return notaRepository.findByTitulo(q);
 	}
 
 }
